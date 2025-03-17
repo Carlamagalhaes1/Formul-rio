da98351fc21acffab3f82e70e357c14e6ab28cdc
Carla Magalhães <carlamagalhes60@gmail.com>
2025-03-17 19:42:05 -0300

    Base do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ function App() {
  
 
   return (
-    <>
+    <div>
+      <h1>carla</h1>
+      <p>Assine nossa NewsLetter e mantenha-se informado</p>
       <div>
-        <p className='text-red-600'> carla</p>
-        <Form/>
-
+        <Form/> 
       </div>
-        
-    </>
+      <p>Ao se inscrever, você passará a receber os nossos e-mails com as dicas, novidades e ofertas!</p>
+    </div>
+    
   )
 }
 
--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,8 +1,22 @@
 const Form = () => {
     return (
-        <div>
-            form
-        </div>
+        <form>
+            <div>
+                <label >Nome:</label>
+                <input type="text" placeholder="Digite seu nome" />
+            </div>
+            <div>
+                <label >Email:</label>
+                <input type="email" placeholder="Digite seu email" />
+            </div>
+            <div>
+                <a href="#">Leia os termos</a>
+                <input type="checkbox"  />
+                <label >Concordo com os termos</label>
+            </div>
+            <button >Cadastrar</button>
+        </form>
+        
     )
 }
 
