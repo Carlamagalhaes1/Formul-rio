df4042b08ab177e6b388386aa0ea934294f45d05
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-07-28 22:05:29 -0300

    add nomes

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -49,7 +49,7 @@ const Form = () => {
     return (
         <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
             <div className="flex flex-col">
-                <label className="text-sm" htmlFor="name" >Nome</label>
+                <label className="text-sm" htmlFor="name" >Nome:</label>
                 
                 <input type="text" placeholder="Digite seu nome" className="rounded-lg py-2 px-2 text-sm placeholder:text-stone-400 border-1 mt-1" value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
